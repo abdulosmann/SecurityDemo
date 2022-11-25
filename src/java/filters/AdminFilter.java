f34fec45a1c3d792bca3aa264081a4dd62692073
abdulosmann <abdulrahman.osman@edu.sait.ca>
2022-11-25 14:50:03 -0700

    1stcommit

---
+++ src/java/filters/AdminFilter.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Filter.java to edit this template
+ */
+package filters;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.*;
+
+/**
+ *
+ * @author aboud
+ */
+@WebFilter(filterName = "AdminFilter", servletNames = {"AdminServlet"})
+public class AdminFilter implements Filter {
+    
+  
+    public void doFilter(ServletRequest request, ServletResponse response,
+            FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        
+        String role = (String) httpRequest.getSession().getAttribute("role");
+        
+        if (role != null && role.equals("admin")){
+           chain.doFilter(request, response); 
+           return;
+        }
+            
+       httpResponse.sendRedirect("notes");
+    
+}
+}
